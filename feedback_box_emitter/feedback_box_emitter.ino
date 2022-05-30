fc4b01255a231168280f6dd20f73cccf8054c26b
celia-bl <blondin.celia@yahoo.fr>
2022-05-30 16:03:07 +0200

    Working leds + buttons
    +Add a file for arduino (wants that the arduino file is in a file with the same name)
    + working leds with the buttons
    - not clean have to clean that a bit

--- feedback_box_emitter/feedback_box_emitter.ino
+++ feedback_box_emitter/feedback_box_emitter.ino
@@ -9,6 +9,7 @@ const int buttons_number = 3;
 int ANALOG_PIN_Q_1 = 1; 
 int ANALOG_PIN_Q_2 = 2; 
 int ANALOG_PIN_Q_3 = 3; 
+int LED_PINS[] = {0,1,2,3,4,5}; 
 int answers[QUESTIONS_NUMBER];   //value on the questions 
 int buttons[QUESTIONS_NUMBER]; //which buttons on the question
 int count[buttons_number]; // decount for debouncing buttons 
@@ -22,6 +23,10 @@ void setup() {
   pinMode(ANALOG_PIN_Q_1, INPUT);
   pinMode(ANALOG_PIN_Q_2, INPUT); 
   pinMode(ANALOG_PIN_Q_3, INPUT); 
+  for (int k=0; k<sizeof(LED_PINS); k++) {
+    pinMode(LED_PINS[k], OUTPUT); 
+  }
+
 
   for (int j=0; j<QUESTIONS_NUMBER; j++) {
     buttons[j] = 0; 
@@ -68,20 +73,29 @@ answers[2] = analogRead(ANALOG_PIN_Q_3);             //Read value on analog pin 
 //for each question determining wich buttons is push
 for (int question =0; question<QUESTIONS_NUMBER; question++) { 
     
-  if (answers[question] > 15 && answers[question] < 180) {        //3 buttons on 1 pin = the difference between them is the value received : between 15 and 180 is the first button
+  if (answers[question] > 50 && answers[question] < 180) {        //3 buttons on 1 pin = the difference between them is the value received : between 15 and 180 is the first button
     debounceButtons(question, 1); 
+    //digitalWrite(0, HIGH); 
+    //Serial.println("led1"); 
          
     }
   else if (answers[question] > 200 && answers[question] < 830) {
     debounceButtons(question, 2); //second button for the question
+    //Serial.println("led2"); 
 
     }
   else if (answers[question] > 940) {
     debounceButtons(question, 3); //third button for the question
-
+    //digitalWrite(2, HIGH);
+    //Serial.println("led3"); 
+ 
     }
   else {
       buttons[question] = 0; //otherwise is 0
+      /*if(millis() - led_time > TURN_ON_TIME) {
+        ledOff(); 
+        Serial.println("switch off lights"); 
+      }*/
     }
   }
 }
@@ -103,6 +117,10 @@ void sendData(int question_number) {
     LoRa.print(packet);
     LoRa.print("!"); 
     LoRa.endPacket();
+    LoRa.beginPacket();
+    LoRa.print("0,1,2");
+    LoRa.print("!"); 
+    LoRa.endPacket();
  
     storingData(question_number,packet); 
     
@@ -122,6 +140,7 @@ void debounceButtons(int question_number, int button_response) {
       count[question_number] = 0;
       buttons[question_number] = button_response;
       sendData(question_number);
+      ledOn(question_number); 
     }
 }
 
@@ -136,4 +155,68 @@ void storingData(int question_number, String packet) {
       Serial.println("error opening the file"); 
     }
     dataFile.close(); 
+}
+
+void ledOn(int question_number) {
+  if(question_number == 0 && buttons[question_number] == 1) {
+    Serial.println("led D0"); 
+    digitalWrite(0, HIGH); 
+    delay(1000); 
+    digitalWrite(0,LOW); 
+  }
+  if(question_number == 0 && buttons[question_number] == 2) {
+    Serial.println("ledD1"); 
+    digitalWrite(1, HIGH); 
+    delay(1000); 
+    digitalWrite(1,LOW); 
+  }
+  if(question_number == 0 && buttons[question_number] == 3) {
+    Serial.println("led D2"); 
+    digitalWrite(2, HIGH); 
+    delay(1000); 
+    digitalWrite(2,LOW); 
+  }
+    if(question_number == 1 && buttons[question_number] == 1) {
+    Serial.println("led D3"); 
+    digitalWrite(3, HIGH); 
+    delay(1000); 
+    digitalWrite(3,LOW); 
+  }
+  if(question_number == 1 && buttons[question_number] == 2) {
+    Serial.println("ledD4"); 
+    digitalWrite(4, HIGH); 
+    delay(1000); 
+    digitalWrite(4,LOW); 
+  }
+  if(question_number == 1 && buttons[question_number] == 3) {
+    Serial.println("led D5"); 
+    digitalWrite(5, HIGH); 
+    delay(1000); 
+    digitalWrite(5,LOW); 
+  }
+    if(question_number == 2 && buttons[question_number] == 1) {
+    Serial.println("led A4"); 
+    analogWrite(19, 255); 
+    delay(1000); 
+    analogWrite(19,0); 
+  }
+  if(question_number == 2 && buttons[question_number] == 2) {
+    Serial.println("ledA5"); 
+    analogWrite(20, 255); 
+    delay(1000); 
+    analogWrite(20,0); 
+  }
+  if(question_number == 2 && buttons[question_number] == 3) {
+    Serial.println("led A6"); 
+    analogWrite(21, 255); 
+    delay(1000); 
+    analogWrite(21,0); 
+  }
+
+}
+
+void ledOff() {
+  digitalWrite(0,LOW); 
+  digitalWrite(1, LOW); 
+  digitalWrite(2, LOW); 
 }
